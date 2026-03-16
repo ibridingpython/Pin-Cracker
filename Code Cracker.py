e92544375b80005ad56529efa583cc62f681b603
ibridingpython <ibprofessionalbusinessacc@gmail.com>
2026-03-16 21:45:52 +0700

    Add files via upload

---
+++ Code Cracker.py
@@ -0,0 +1,63 @@
+from random import choice
+from itertools import product 
+import time 
+
+#? itertools is to make sure that it'll try each combination once
+#? making the brute force extremely effective
+
+#* A simulation of pin cracking program.
+
+prompt = ("Welcome to pin cracker!")
+prompt += ("\nPlease insert the length of the pin: ")
+
+digits = list(range(10))
+attempts = 0
+bruting = True
+
+while bruting == True:
+    password = input(prompt)
+    password = int(password)
+    
+    generated_pin = []
+    
+    for i in range(password):
+        value = choice(digits)
+        generated_pin.append(value)
+        
+    print(f"{len(generated_pin)} digits pin has been succesfully generated.")
+    print(f"Program will now attempt to brute force.")
+    print()
+    
+        #! countdown
+    for i in range(3, 0, -1):
+        print(i)
+        time.sleep(1)
+            
+    print("Starting attack...")
+    start_time = time.time() #! time
+    bruting = False
+    
+    
+    
+
+for attack_pin in product(digits, repeat=password):
+        
+        attempts += 1
+        
+        if attempts % 1000000 == 0:
+            print(f"Attempts so far: {attempts}")
+            print(f"Current guess:", "".join(str(x) for x in attack_pin))
+            
+        if list(attack_pin) == generated_pin:
+            
+            elapsed_time = time.time() - start_time
+            attempts_per_second = attempts / elapsed_time
+            
+            print("Attack successfully executed.")
+            print("> The pin is:", "".join(str(x) for x in attack_pin))
+            print("> Attempts taken:", attempts)
+            print("> Attempts per second:", f"{attempts_per_second:.2f}")
+            print("> Time taken:", f"{elapsed_time:.2f}")
+            print()
+
+            break
